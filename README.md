08d9451fb733ffc297b7501ff1363019ccfb7c6c
Oscar Blanco <oscarmb86@hotmail.com>
2022-04-22 11:46:56 -0600

    se hizo uso de la herramienta de webpack-bundle-analyzer para conocer detalles de la cantidad de codigo utilizado

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 Variable de entorno en uso:
  API=https://randomuser.me/api/
+
+
+
+Clase 20 webpack bundle analyzer
+
+npm install webpack-bundle-analyzer -D
+
+webpack dev
+
+                Consts:
+                const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+
+
+                Plugins: 
+                new BundleAnalyzerPlugin(),
+
+
+npx webpack --profile --json > stats
+npx webpack-bundle-analyzer
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 // const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 // const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 
 module.exports = {
@@ -79,6 +80,7 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
 //   optimization: {
 //     minimizer: true,
